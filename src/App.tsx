c167f9d2f6be4ae739979ee136118348d2527cc7
webcuser <97110714+webcuser@users.noreply.github.com>
2026-06-24 15:00:25 +0200

    Initialize React project with TypeScript using Vite

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Fleet Rental Management Demo</h1>
+      <p>React + TypeScript project is running!</p>
+    </div>
+  );
+}
+
+export default App;
